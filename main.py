a4461bf2fea27457dd4291211084b6ee4c503946
trustworthyaxolotl26 <efreeman2029@kehillahstudent.org>
2025-09-11 09:24:22 -0700

    message

--- main.py
+++ main.py
@@ -1,3 +1,31 @@
 import turtle
 
-turtle.forward(100)
+st = 80
+
+def curve(f, r):
+    for i in range(4):
+        turtle.forward(f)
+        turtle.right(r)
+    turtle.right(15)
+
+turtle.speed(15)
+
+turtle.left(195)
+curve(30, 10)
+for i in range (2):
+    turtle.forward(30)
+    turtle.right(90)
+    turtle.forward(30)
+    turtle.left(90)
+turtle.right(50)
+turtle.forward(st)
+turtle.right(130)
+turtle.forward(30)
+turtle.left(50)
+curve(20, 7)
+# turtle.right(50)
+
+
+
+
+turtle.exitonclick()

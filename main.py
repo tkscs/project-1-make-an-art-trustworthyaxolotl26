6f4997bc417c03c87cb2fa02c2d666db5db3c874
trustworthyaxolotl26 <efreeman2029@kehillahstudent.org>
2025-09-26 11:56:22 -0700

    basicly done. not opimized tho

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import turtle
 
 scale = 10
-#only 10 for now
+#any numbers
 
 def curve(f, r):
     for i in range(4):
@@ -11,65 +11,72 @@ def curve(f, r):
 
 def earI(f):
 #1.5 for best results, any for goofy results
+#1.4 works good to.
     turtle.forward(f)
     turtle.left(120)
-    turtle.forward(f-5)
+    turtle.forward(f-(scale*0.5))
     turtle.left(100)
-    turtle.forward(f-1.5)
+    turtle.forward(f-(scale*.15))
     turtle.left(120)
 
-turtle.speed(2)
+turtle.speed(15)
+
+def cheekFuz():
+    for i in range (2):
+        turtle.forward(scale*3)
+        #print(turtle.position())
+        turtle.right(90)
+        turtle.forward(scale*3)
+        turtle.left(90)
 
 def draw_face_inside_curves():
     turtle.goto(-scale*12.5, scale*8.5)
     turtle.down()
     turtle.left(22)
-    curve(scale*2.03, 5)
+    curve(scale*2.03, 5.9)
+    #print(turtle.heading())
     turtle.up()
     turtle.goto(-scale*0.1, scale*8.5)
     turtle.down()
-    turtle.right(92)
-    curve(scale*1.75, -5)
-    print(turtle.position())
+    turtle.right(91.4)
+    curve(scale*1.73, -6)
+    #print(turtle.heading())
 
+#bottom of face 
 turtle.left(195)
-print(turtle.heading())
+#print(turtle.heading())
 curve(scale*3, 10)
-print(turtle.heading())
-for i in range (2):
-    turtle.forward(scale*3)
-    print(turtle.position())
-    turtle.right(90)
-    turtle.forward(scale*3)
-    turtle.left(90)
+#print(turtle.heading())
 
+#l cheek
+cheekFuz()
+#l top face
 turtle.right(50)
 turtle.forward(scale*8)
 turtle.right(130)
 turtle.forward(scale*3)
 turtle.left(50)
+#crown of head 
 curve(scale*2, 7)
+#r top face
 turtle.left(80)
 turtle.forward(scale*3)
 turtle.right(136)
 turtle.forward(scale*8+2)
 turtle.left(45)
-for i in range (2):
-    turtle.forward(scale*3)
-    print(turtle.position())
-    turtle.right(90)
-    turtle.forward(scale*3)
-    turtle.left(90)
+#r cheek
+cheekFuz()
 turtle.up()
 #face inside curves
 draw_face_inside_curves()
 turtle.up()
 #nose dash
-turtle.goto(-scale*5,scale*4.8)
+turtle.goto(-scale*5.15,scale*4.8)
 turtle.right(35)
 turtle.down()
-turtle.forward(scale*1.2)
+turtle.forward(scale*0.9)
 turtle.up()
+#ear inside start
 turtle.goto(-scale*12.3, scale*16)
 turtle.left(90)
 turtle.down()
@@ -80,11 +87,13 @@ turtle.right(21)
 turtle.down()
 earI(scale*1.5)
 turtle.up()
+#eyes
 turtle.goto(-scale*2, scale*5)
-turtle.dot()
+turtle.dot(scale)
 turtle.goto(-scale*10, scale*5)
-turtle.dot()
-turtle.goto(-scale*5.5, scale*4.2)
+turtle.dot(scale)
+#mouth (ish)
+turtle.goto(-scale*5.56, scale*4.35)
 turtle.left(61)
 turtle.down()
 turtle.forward(scale*2)
